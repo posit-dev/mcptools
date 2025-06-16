posit-dev/mcptools

commit 1436b0a7b962b7938d114c712ec7ab95193e83d1

implement `mcp_server(tools)` (#40)

R/tools.R
@@ -1,53 +1,4 @@
-#' Set the tools available to run in your R session
-#'
-#' @description
-#' By default, acquaint supplies tools from [btw::btw_tools()] to allow clients
-#' to peruse package documentation, inspect your global environment, and query
-#' session details. This function allows you register any tools created with
-#' [ellmer::tool()] instead.
-#'
-#' A call to this function must be placed in your `.Rprofile` and the client
-#' (i.e. Claude Desktop or Claude Code) restarted in order for the new tools
-#' to be registered.
-#'
-#' acquaint will always register the tools "list_r_sessions" and
-#' "select_r_session" in addition to the tools provided here; those tool names
-#' are thus reserved for the package.
-#'
-#' @param x A list of tools created with [ellmer::tool()]. Any list that could
-#' be passed to `chat$set_tools()` can be passed here.
-#'
-#' @returns
-#' `x`, invisibly. Called for side effects. The function will error if `x` is
-#' not a list of `ellmer::ToolDef` objects or if any tool name is one of the
-#' reserved names "list_r_sessions" or "select_r_session".
-#'
-#' @examples
-#' library(ellmer)
-#'
-#' tool_rnorm <- tool(
-#'   rnorm,
-#'   "Draw numbers from a random normal distribution",
-#'   n = type_integer("The number of observations. Must be a positive integer."),
-#'   mean = type_number("The mean value of the distribution."),
-#'   sd = type_number("The standard deviation of the distribution. Must be a non-negative number.")
-#' )
-#'
-#' # supply only one tool, tool_rnorm
-#' mcp_set_tools(list(tool_rnorm))
-#'
-#' # supply both tool_rnorm and `btw_tools()`
-#' mcp_set_tools(c(list(tool_rnorm), btw::btw_tools()))
-#' @export
-mcp_set_tools <- function(x) {
-  check_acquaint_tools(x)
-
-  options(.acquaint_tools = x)
-
-  invisible(x)
-}
-
-check_acquaint_tools <- function(x, call = caller_env()) {
+set_server_tools <- function(x, call = caller_env()) {
   if (!is_list(x) || !all(vapply(x, inherits, logical(1), "ellmer::ToolDef"))) {
     msg <- "{.arg x} must be a list of tools created with {.fn ellmer::tool}."
     if (inherits(x, "ellmer::ToolDef")) {
@@ -68,6 +19,14 @@ check_acquaint_tools <- function(x, call = caller_env()) {
       call = call
     )
   }
+
+  the$server_tools <- c(
+    x,
+    list(
+      list_r_sessions_tool,
+      select_r_session_tool
+    )
+  )
 }
 
 # These two functions are supplied to the client as tools and allow the client
@@ -86,9 +45,10 @@ list_r_sessions <- function() {
         autostart = NA,
         fail = "none"
       ) &&
-      i > 8L
-    )
+        i > 8L
+    ) {
       break
+    }
   }
   pipes <- nanonext::read_monitor(monitor)
   res <- lapply(
@@ -97,7 +57,7 @@ list_r_sessions <- function() {
   )
   lapply(
     pipes,
-    function(x) nanonext::send_aio(sock, "", mode = "raw", pipe = x)
+    function(x) nanonext::send_aio(sock, character(), mode = "serial", pipe = x)
   )
   sort(as.character(nanonext::collect_aio_(res)))
 }
@@ -147,13 +107,8 @@ select_r_session_tool <-
   )
 
 get_acquaint_tools <- function() {
-  res <- c(
-    getOption(".acquaint_tools", default = btw::btw_tools()),
-    list(
-      list_r_sessions_tool,
-      select_r_session_tool
-    )
-  )
+  # must be called inside of the server session
+  res <- the$server_tools
   set_names(res, vapply(res, \(x) x@name, character(1)))
 }
 
@@ -165,18 +120,8 @@ get_acquaint_tools_as_json <- function() {
 
 execute_tool_call <- function(data) {
   tool_name <- data$params$name
-  
-  tools <- get_acquaint_tools()
-  if (!tool_name %in% names(tools)) {
-    return(jsonrpc_response(
-      data$id,
-      error = list(code = -32601, message = "Method not found")
-    ))
-  }
-  
-  fn <- tools[[tool_name]]@fun
   args <- data$params$arguments
-  
+
   # HACK for btw_tool_env_describe_environment. In the JSON, it will have
   # `"items": []`, and that translates to an empty list, but we want NULL.
   if (tool_name == "btw_tool_env_describe_environment") {
@@ -184,9 +129,9 @@ execute_tool_call <- function(data) {
       args$items <- NULL
     }
   }
-  
+
   tryCatch(
-    as_tool_call_result(data, do.call(fn, args)),
+    as_tool_call_result(data, do.call(data$tool, args)),
     error = function(e) {
       jsonrpc_response(
         data$id,

NAMESPACE
@@ -2,5 +2,4 @@
 
 export(mcp_server)
 export(mcp_session)
-export(mcp_set_tools)
 import(rlang)

R/server.R
@@ -1,11 +1,37 @@
 # The MCP server is a proxy. It takes input on stdin, and when the input forms
 # valid JSON, it will send the JSON to the session. Then, when it receives the
 # response, it will print the response to stdout.
+#' @param tools A list of tools created with [ellmer::tool()] that will be
+#' available from the server. Any list that could be passed to `Chat$set_tools()`
+#' can be passed here. By default, the package will use [btw::btw_tools()].
+#'
 #' @rdname mcp
 #' @export
-mcp_server <- function() {
+#'
+#' @examples
+#' # should only be run non-interactively, and will block the current R process
+#' # once called.
+#' if (FALSE) {
+#' # to just start a server with btw tools:
+#' mcp_server()
+#'
+#' # to do so with non-default tools:
+#' library(ellmer)
+#'
+#' tool_rnorm <- tool(
+#'   rnorm,
+#'   "Draw numbers from a random normal distribution",
+#'   n = type_integer("The number of observations. Must be a positive integer."),
+#'   mean = type_number("The mean value of the distribution."),
+#'   sd = type_number("The standard deviation of the distribution. Must be a non-negative number.")
+#' )
+#'
+#' mcp_server(tools = list(tool_rnorm))
+#' }
+mcp_server <- function(tools = btw::btw_tools()) {
   # TODO: should this actually be a check for being called within Rscript or not?
   check_not_interactive()
+  set_server_tools(tools)
 
   cv <- nanonext::cv()
   reader_socket <- nanonext::read_stdin()
@@ -83,17 +109,16 @@ handle_message_from_client <- function(line) {
       tool_name %in%
         c("list_r_sessions", "select_r_session") ||
         # with no sessions available, just execute tools in the server (#36)
-      !nanonext::stat(the$server_socket, "pipes")
+        !nanonext::stat(the$server_socket, "pipes")
     ) {
       handle_request(data)
     } else {
-      result <- forward_request(line)
+      result <- forward_request(data)
     }
   } else if (is.null(data$id)) {
     # If there is no `id` in the request, then this is a notification and the
     # client does not expect a response.
-    if (data$method == "notifications/initialized") {
-    }
+    if (data$method == "notifications/initialized") {}
   } else {
     cat_json(jsonrpc_response(
       data$id,
@@ -114,9 +139,15 @@ handle_message_from_session <- function(data) {
 }
 
 forward_request <- function(data) {
-  logcat(c("TO SESSION: ", data))
+  logcat(c("TO SESSION: ", jsonlite::toJSON(data)))
 
-  nanonext::send_aio(the$server_socket, data, mode = "raw")
+  prepared <- append_tool_fn(data)
+
+  if (inherits(prepared, "jsonrpc_error")) {
+    return(prepared)
+  }
+
+  nanonext::send_aio(the$server_socket, prepared, mode = "serial")
 }
 
 # This process will be launched by the MCP client, so stdout/stderr aren't
@@ -188,7 +219,38 @@ check_not_interactive <- function(call = caller_env()) {
 }
 
 handle_request <- function(data) {
-  result <- execute_tool_call(data)
+  prepared <- append_tool_fn(data)
+
+  if (inherits(prepared, "jsonrpc_error")) {
+    result <- prepared
+  } else {
+    result <- execute_tool_call(prepared)
+  }
+
   logcat(c("FROM SERVER: ", to_json(result)))
   cat_json(result)
+}
+
+# the session needs access to the function called by the server; in addition
+# to the raw jsonrpc request, append the relevant R function if the request
+# is a `tools/call`
+append_tool_fn <- function(data) {
+  if (!identical(data$method, "tools/call")) {
+    return(data)
+  }
+
+  tool_name <- data$params$name
+
+  if (!tool_name %in% names(get_acquaint_tools())) {
+    return(structure(
+      jsonrpc_response(
+        data$id,
+        error = list(code = -32601, message = "Method not found")
+      ),
+      class = "jsonrpc_error"
+    ))
+  }
+
+  data$tool <- get_acquaint_tools()[[tool_name]]@fun
+  data
 }

R/session.R
@@ -37,8 +37,6 @@
 #' available to the server.
 #'
 #' @seealso
-#' - [mcp_set_tools()] allows you to register any ellmer tools as the tools
-#' that will be provided to the client.
 #' - The "Getting started with acquaint" vignette at
 #' `vignette("acquaint", package = "acquaint")` delves into further detail
 #' on setup and customization.
@@ -76,12 +74,11 @@ mcp_session <- function() {
   schedule_handle_message_from_server()
 }
 
-handle_message_from_server <- function(msg) {
+handle_message_from_server <- function(data) {
   pipe <- nanonext::pipe_id(the$raio)
   schedule_handle_message_from_server()
 
-  # cat("RECV :", msg, "\n", sep = "", file = stderr())
-  if (!nzchar(msg)) {
+  if (length(data) == 0) {
     return(
       nanonext::send_aio(
         the$session_socket,
@@ -91,7 +88,6 @@ handle_message_from_server <- function(msg) {
       )
     )
   }
-  data <- jsonlite::parse_json(msg)
 
   if (data$method == "tools/call") {
     body <- execute_tool_call(data)
@@ -133,7 +129,7 @@ as_tool_call_result <- function(data, result) {
 }
 
 schedule_handle_message_from_server <- function() {
-  the$raio <- nanonext::recv_aio(the$session_socket, mode = "string")
+  the$raio <- nanonext::recv_aio(the$session_socket, mode = "serial")
   promises::as.promise(the$raio)$then(handle_message_from_server)$catch(
     \(e) {
       # no op but ensures promise is never rejected

man/mcp.Rd
@@ -6,9 +6,14 @@
 \alias{mcp_session}
 \title{Model context protocol for your R session}
 \usage{
-mcp_server()
+mcp_server(tools = btw::btw_tools())
 
 mcp_session()
+}
+\arguments{
+\item{tools}{A list of tools created with \code{\link[ellmer:tool]{ellmer::tool()}} that will be
+available from the server. Any list that could be passed to \code{Chat$set_tools()}
+can be passed here. By default, the package will use \code{\link[btw:btw_tools]{btw::btw_tools()}}.}
 }
 \description{
 Together, these functions implement a model context protocol server for your
@@ -47,6 +52,25 @@ available to the server.
 }
 
 \examples{
+# should only be run non-interactively, and will block the current R process
+# once called.
+if (FALSE) {
+# to just start a server with btw tools:
+mcp_server()
+
+# to do so with non-default tools:
+library(ellmer)
+
+tool_rnorm <- tool(
+  rnorm,
+  "Draw numbers from a random normal distribution",
+  n = type_integer("The number of observations. Must be a positive integer."),
+  mean = type_number("The mean value of the distribution."),
+  sd = type_number("The standard deviation of the distribution. Must be a non-negative number.")
+)
+
+mcp_server(tools = list(tool_rnorm))
+}
 if (interactive()) {
 mcp_session()
 }
@@ -54,8 +78,6 @@ mcp_session()
 }
 \seealso{
 \itemize{
-\item \code{\link[=mcp_set_tools]{mcp_set_tools()}} allows you to register any ellmer tools as the tools
-that will be provided to the client.
 \item The "Getting started with acquaint" vignette at
 \code{vignette("acquaint", package = "acquaint")} delves into further detail
 on setup and customization.

man/mcp_set_tools.Rd
@@ -1,48 +1,0 @@
-% Generated by roxygen2: do not edit by hand
-% Please edit documentation in R/tools.R
-\name{mcp_set_tools}
-\alias{mcp_set_tools}
-\title{Set the tools available to run in your R session}
-\usage{
-mcp_set_tools(x)
-}
-\arguments{
-\item{x}{A list of tools created with \code{\link[ellmer:tool]{ellmer::tool()}}. Any list that could
-be passed to \code{chat$set_tools()} can be passed here.}
-}
-\value{
-\code{x}, invisibly. Called for side effects. The function will error if \code{x} is
-not a list of \code{ellmer::ToolDef} objects or if any tool name is one of the
-reserved names "list_r_sessions" or "select_r_session".
-}
-\description{
-By default, acquaint supplies tools from \code{\link[btw:btw_tools]{btw::btw_tools()}} to allow clients
-to peruse package documentation, inspect your global environment, and query
-session details. This function allows you register any tools created with
-\code{\link[ellmer:tool]{ellmer::tool()}} instead.
-
-A call to this function must be placed in your \code{.Rprofile} and the client
-(i.e. Claude Desktop or Claude Code) restarted in order for the new tools
-to be registered.
-
-acquaint will always register the tools "list_r_sessions" and
-"select_r_session" in addition to the tools provided here; those tool names
-are thus reserved for the package.
-}
-\examples{
-library(ellmer)
-
-tool_rnorm <- tool(
-  rnorm,
-  "Draw numbers from a random normal distribution",
-  n = type_integer("The number of observations. Must be a positive integer."),
-  mean = type_number("The mean value of the distribution."),
-  sd = type_number("The standard deviation of the distribution. Must be a non-negative number.")
-)
-
-# supply only one tool, tool_rnorm
-mcp_set_tools(list(tool_rnorm))
-
-# supply both tool_rnorm and `btw_tools()`
-mcp_set_tools(c(list(tool_rnorm), btw::btw_tools()))
-}

tests/testthat/test-tools.R
@@ -1,33 +1,0 @@
-test_that("mcp_set_tools works", {
-  old_option <- getOption("acquaint_tools")
-  on.exit(options(.acquaint_tools = old_option))
-
-  # must be a list
-  expect_snapshot(error = TRUE, mcp_set_tools("boop"))
-
-  tool_rnorm <- ellmer::tool(
-    rnorm,
-    "Draw numbers from a random normal distribution",
-    n = ellmer::type_integer(
-      "The number of observations. Must be a positive integer."
-    ),
-    mean = ellmer::type_number("The mean value of the distribution."),
-    sd = ellmer::type_number(
-      "The standard deviation of the distribution. Must be a non-negative number."
-    )
-  )
-  tool_rnorm_list <- list(tool_rnorm)
-
-  # tools themselves need to be in a list
-  expect_snapshot(error = TRUE, mcp_set_tools(tool_rnorm))
-
-  # uses reserved name
-  tool_rnorm@name <- "list_r_sessions"
-  expect_snapshot(error = TRUE, mcp_set_tools(list(tool_rnorm)))
-
-  expect_equal(mcp_set_tools(tool_rnorm_list), tool_rnorm_list)
-  expect_equal(
-    names(get_acquaint_tools()),
-    c("rnorm", "list_r_sessions", "select_r_session")
-  )
-})

vignettes/acquaint.Rmd
@@ -65,8 +65,8 @@ Users might be interested in extending these tools or supplying completely diffe
 -   A set of tools specifically focused on R package development (e.g. various wrappers of [devtools](https://devtools.r-lib.org/) and [testthat](https://testthat.r-lib.org/)) that could be hooked up to Claude Code to make a package development assistant.
 -   For the brave, a tool `run_r_code()` that allows a client to run whatever R code it wants.
 
-You can configure any set of tools that could be passed to the `$set_tools()` method of an ellmer Chat object as the tools that acquaint will supply to clients. To do so, supply a list of outputs from `ellmer::tool()` to `acquaint::mcp_set_tools()` *inside of your .Rprofile*. It's important that `mcp_set_tools()` is called inside of your .Rprofile so that it's run every time R starts. That way, when clients start and run the configured command `Rscript`, `Rscript` will first run `mcp_set_tools()` so that, when the client asks the server what tools are available, the server can describe specifically the new tools you've configured. Relatedly, you'll need to make sure that your code that specifies the new tools can run in a fresh R session; be sure to either namespace functions from libraries (like `ellmer::tool()` instead of `tool()`) or load the libraries entirely.
+You can configure any set of tools that could be passed to the `$set_tools()` method of an ellmer Chat object as the tools that acquaint will supply to clients. To do so, supply a list of outputs from `ellmer::tool()` to the `tools` argument of `mcp_server()`. Relatedly, you'll need to make sure that your code that specifies the new tools can run in a fresh R session; be sure to either namespace functions from libraries (like `ellmer::tool()` instead of `tool()`) or load the libraries entirely.
 
 <!--# \[TODO: provide an example of registering the `rnorm()` tool.\] -->
 
-Note that, if you'd like to supply some custom tools *in addition to* the default tools from btw, you'll need to write `acquaint::mcp_set_tools(c(btw::btw_tools(), your_custom_tools))`.
+Note that, if you'd like to supply some custom tools *in addition to* the default tools from btw, you'll need to set `tools = (c(btw::btw_tools(), your_custom_tools))`.

commit c89178ffb9fa2f2f43b74b932fa4963ce06a7d7d

address errors with array parameters (closes #42)
I introduced this on Friday in 1436b0a7b962b7938d114c712ec7ab95193e83d1 when adding the serialization layer required to pass R functions between the server and session. With the previous commit, could reproduce with:

```
devtools::load_all()

tools <- btw::btw_tools()
set_server_tools(tools)

data <- list()
data$tool <- get_acquaint_tools()[["btw_tool_session_package_info"]]@fun

# the packages argument is coming in as a list when read from json
args <- list(packages = list("loaded"))
do.call(data$tool, args)
Error in `order()`:
! argument 2 is not a vector
```

The issue is that `type_array` arguments are not auto-unboxed when unserialized in the session.

R/tools.R
@@ -130,6 +130,14 @@ execute_tool_call <- function(data) {
     }
   }
 
+  args <- lapply(args, function(x) {
+    if (is.list(x) && is.null(names(x))) {
+      unlist(x, use.names = FALSE)
+    } else {
+      x
+    }
+  })
+
   tryCatch(
     as_tool_call_result(data, do.call(data$tool, args)),
     error = function(e) {